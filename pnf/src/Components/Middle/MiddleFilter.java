16c652157805424ed4926e060044405e68237919
sujk912 <aa991122aa@gmail.com>
2022-11-16 00:57:20 +0900

    PNFA-2

--- pnf/src/Components/Middle/MiddleFilter.java
+++ pnf/src/Components/Middle/MiddleFilter.java
@@ -28,16 +28,17 @@ public class MiddleFilter extends CommonFilterImpl{
                 if(byte_read == ' ') numOfBlank++;
                 if(byte_read != -1) buffer[idx++] = (byte)byte_read;
                 // CS가 필요하다고 했을때 CS가 있거나, CS가 필요없다고 했을 때 CS가 없다면 isPass를 true로 바꿔준다.
-                if((this.isCSMustContain && numOfBlank == checkBlank && buffer[idx-3] == 'C' && buffer[idx-2] == 'S')
-                    ||(!this.isCSMustContain && numOfBlank == checkBlank && buffer[idx-3] != 'C' && buffer[idx-2] != 'S'))
-                    isPass = true;
-            }      
+                if(this.isCSMustContain && numOfBlank == checkBlank && buffer[idx-3] == 'C' && buffer[idx-2] == 'S') isPass = true;
+                if(!this.isCSMustContain && numOfBlank == checkBlank && buffer[idx-3] != 'C' && buffer[idx-2] != 'S'
+                        && Character.isLetter((char)buffer[idx-3]) && Character.isLetter((char)buffer[idx-2])) isPass = true;
+            }
             if(isPass) {
-                for(int i = 0; i<idx; i++) 
+                for(int i = 0; i<idx; i++){
                     out.write((char)buffer[i]);
+                }
                 isPass = false; // reset
             }
-            if (byte_read == -1) return true;
+            if (byte_read == -1)  return true;
             idx = 0;
             numOfBlank = 0;
             byte_read = '\0';
--- pnf/src/Framework/LifeCycleManager.java
+++ pnf/src/Framework/LifeCycleManager.java
@@ -14,25 +14,21 @@ public class LifeCycleManager {
             CommonFilter sourceFilter = new SourceFilter("Students.txt");
             CommonFilter sinkFilter = new SinkFilter("Output.txt");
             CommonFilter middleFilter = new MiddleFilter(true);
-            CommonFilter add12345Filter = new AddCourseMiddleFilter("12345");
-            CommonFilter add23456Filter = new AddCourseMiddleFilter("23456");
+            CommonFilter add17651Filter = new AddCourseMiddleFilter("17651");
 
             sourceFilter.connectOutputTo(middleFilter);
-            middleFilter.connectOutputTo(add12345Filter);
-            add12345Filter.connectOutputTo(add23456Filter);
-            add23456Filter.connectOutputTo(sinkFilter);
-            
+            middleFilter.connectOutputTo(add17651Filter);
+            add17651Filter.connectOutputTo(sinkFilter);
+
             Thread thread1 = new Thread(sourceFilter);
             Thread thread2 = new Thread(sinkFilter);
             Thread thread3 = new Thread(middleFilter);
-            Thread thread4 = new Thread(add12345Filter);
-            Thread thread5 = new Thread(add23456Filter);
-            
+            Thread thread4 = new Thread(add17651Filter);
+
             thread1.start();
             thread2.start();
             thread3.start();
             thread4.start();
-            thread5.start();
         } catch(Exception e) {
             e.printStackTrace();
         }
